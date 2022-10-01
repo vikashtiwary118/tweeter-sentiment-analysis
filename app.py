117aaa833ee7e20ac461d01ea9aff20471640814
vikash tiwary <tiwaryvikash118@gmail.com>
2022-10-01 14:13:36 -0700

    added updated changes

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ import sentiment_analysis
 import json
 app = Flask(__name__)
 
+def fetchDataFromAPI(text):
+	list_data=[{'tweet':'Wah modi ji Wah', 'sentiment': 'positive'}, {'tweet':'India loss the match', 'sentiment': 'negative'}, {'tweet':'I am going to delhi with my friend', 'sentiment': 'neutarl'}]
+	return list_data
+
 @app.route('/')
 def hello_world():
-	return render_template("index.html", rows=json.dumps([]))
+    return render_template("index.html")
 
-@app.route('/tweeter', methods=['POST'])
+@app.route('/', methods=['POST'])
 def searched_tweet():
 	if request.method == 'POST':
-	    text = request.json['searched']
+	    text = request.form['searched']
 	    if text:
 		    data_list=sentiment_analysis.sentimental_analysis(text)
-		    number_of_page=len(data_list)//10
-		    list_number_of_page=[i for i in range(1,number_of_page)]
-		    return json.dumps(data_list)
+		    return render_template('response.html', rows=json.dumps(data_list))
 	return redirect('/')
 
 
