586807b2930cebbcf84069af193cf68c914abb80
vikash tiwary <tiwaryvikash118@gmail.com>
2022-10-01 23:29:14 -0700

    first commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 ## api removed from public code please use your own api
-consumer_key = 'BUo1mEXl9gLmY5XyZfxc3zO3W'
-consumer_key_s = 'UzHZuf7AFCFqOV9MdtsJZnTTPz4mjN7RV98ZjldwlqA8BLiz8W'
-access_token = '1573312881172496384-RlpGgvjRFTFEN8JGZqZID9jejwVvzL'
-access_token_s = 'XVNb6XgmTQCpYv4O4g2Y5VxFLz3TWxdBuIJqVVATfXPh8'
+consumer_key = ''
+consumer_key_s = ''
+access_token = ''
+access_token_s = ''
